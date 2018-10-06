da5d293463c028498ad9faf5082ec905f71b6594
RmrGnzlzLpz <rmrgnzlzlpz@gmail.com>
2018-10-06 00:21:05 -0500

    Estructura de los modelos "Finca" y "Perimetro"

---
+++ application/models/Fincas/Fincas_Model.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Fincas_Model extends CI_Model {
+    public function consultarGeneral() {
+        /**
+         * Mostrar los datos de todas las fincas.
+         */
+    }
+
+    public function consultarIndividual($idFinca) {
+        /**
+         * Consultar los datos de una finca, por su id.
+         */
+    }
+
+    public function consultarPorDepartmento($nombreDpto) {
+        /**
+         * Consultar los datos de fincas en un departamento.
+         */
+    }
+
+    public function consultarPorCiudad($nombreCiudad) {
+        /**
+         * Consultar los datos de fincas por ciudades o municipios.
+         */
+    }
+
+    public function nuevaFinca() {
+        
+    }
+}
